b5663a2f82a72b67f7db6f578ee1987e1ae1a196
erodrig87 <erodrig87@gmail.com>
2022-08-23 10:10:01 -0300

    local storage

---
+++ js/Gasto.js
@@ -0,0 +1,33 @@
+// Def clase Gastos, para crear objetos de tipo clase de gastos.
+class Gasto {
+    constructor(fecha, categoria, valor, remito, pago) {
+        this.fecha = fecha;
+        this.categoria = categoria;
+        this.valor = valor;
+        this.remito = remito;
+        this.pago = pago;
+        this.ID = `ID_GASTO_${ID_GASTO_GLOBAL}`;
+
+    }
+    //metodo retorna nodo de celda de tabal segun parametro atributo
+    returnNodoCeldaTabla(atributo) {
+        let nodo = document.createElement('td');
+        nodo.innerText = `${this[atributo]}`;
+        return nodo;
+    }
+    deleteCompleteRow() {
+        let row = document.getElementById(this.ID);
+        row.parentNode.removeChild(row);
+    }
+    storeExpense() {
+        // Parse any JSON previously stored in allEntries
+        let arrExpensesStored = JSON.parse(localStorage.getItem("arrExpensesStored"));
+        if(arrExpensesStored == null) arrExpensesStored = [];
+       //localStorage.setItem(this.ID, JSON.stringify(this));
+        // Save allEntries back to local storage
+        arrExpensesStored.push(this);
+        localStorage.setItem("arrExpensesStored", JSON.stringify(arrExpensesStored));
+        localStorage.setItem("ID_GASTO_GLOBAL", JSON.stringify(ID_GASTO_GLOBAL));
+        ID_GASTO_GLOBAL++;
+    };
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Mensaje de informacion
-alert(
+/*alert(
 `Se ingresan en automaticamente 9 gastos en 3 categorias diferentes para facilitar pruebas y visualizacion.
  
 Se agregan eventos:
@@ -13,54 +13,42 @@ Interaccion con HTML:
         
 Sin interaccion con HTML:
 -Boton 'Mostrar Calculos', muestra gasto maximo, minimo y promedio total.
--Boton 'Filtrar Categoria', solicita ingresar categoria a filtrar y muestra gastos.`);
+-Boton 'Filtrar Categoria', solicita ingresar categoria a filtrar y muestra gastos.`);*/
 
 //Def variables globales
 let gasto_total = 0;
-let promedio = 0;
-let gasto_max = 0;
-let gasto_min = 0;
-let ID_GASTO_GLOBAL = 0;
+let promedio = parseFloat(JSON.parse(localStorage.getItem("AVERAGE_EXPENSES")) || 0);
+let gasto_max = parseFloat(JSON.parse(localStorage.getItem("MAX_EXPENSE")) || 0);
+let gasto_min = parseFloat(JSON.parse(localStorage.getItem("MIN_EXPENSE")) || 0);
+let ID_GASTO_GLOBAL = JSON.parse(localStorage.getItem("ID_GASTO_GLOBAL")) || 0;
+let arrExpensesStored= JSON.parse(localStorage.getItem("arrExpensesStored")) || [];
+let arrayGastos = [];
 let selectedFecha;
 let selectedValue;
 
-//Def array para almacenar objetos creados de clase Gasto
-let arrayGastos = [];
+arrExpensesStored.length>0 && updateArrayExpenseTable();
 
-// Def clase Gastos, para crear objetos de tipo clase de gastos.
-class Gasto {
-    constructor(fecha, categoria, valor,remito,pago) {
-        this.fecha = fecha;
-        this.categoria = categoria;
-        this.valor = valor;
-        this.remito = remito;
-        this.pago = pago;
-        this.ID = `ID_GASTO_${ID_GASTO_GLOBAL++}`;
-    }
-    //metodo retorna nodo de celda de tabal segun parametro atributo
-    returnNodoCeldaTabla(atributo)
-    {
-        let nodo =document.createElement('td');
-        nodo.innerText=`${this[atributo]}`;
-        return nodo;
-    }
-    deleteCompleteRow()  
-    {     
-        let row = document.getElementById(this.ID);
-        row.parentNode.removeChild(row);
-    }
-    //boolCategoria = (check_categoria) => (this.categoria == check_categoria) ? true : false; Metodo devuelve bool segun valor categoria || no utilizado
-}
+function updateArrayExpenseTable(){
 
-//Funcion mensaje estado variables globales
-function alertStatus() {
-    alert(`
-    El promedio de los gastos ingresados es: ${promedio.toFixed(2)}
-    El gasto maximo fue de: ${gasto_max.toFixed(2)}
-    El gasto minimo fue de: ${gasto_min.toFixed(2)}
-    El gasto total fue de: ${gasto_total.toFixed(2)}
-    Cantidad de gastos ingresados: ${arrayGastos.length}`);
+    let fecha,categoria,valor,remito,pago;
+
+    arrExpensesStored.forEach(function(expenseStored) {
+        fecha = new Date();  
+        fecha = fecha.toLocaleDateString();
+        categoria = expenseStored.categoria;
+        valor = expenseStored.valor;
+        remito = expenseStored.remito;
+        pago = expenseStored.pago;
+
+        let nuevoGasto = new Gasto(fecha, categoria, valor,remito,pago);
+        arrayGastos.push(nuevoGasto);
+
+        document.getElementById("inputExpenseForm").reset();
+        
+        addRowTable(nuevoGasto);
+     })    
 }
+//randomExpenses();
 
 //Funcion resetea variables globales y elimina objetos del array de gastos
 const resetGastos = (dias) => {
@@ -74,12 +62,10 @@ const resetGastos = (dias) => {
          arrayGastos.pop();
     }
     ID_GASTO_GLOBAL = 0;
-    alertStatus();
-
 }
 
 //funcion que devuelve fecha actual - x dias pasados como parametro utilizada en dashboard.js para modificar eje x del grafico
-const restarDias = (dias) => {
+function restarDias(dias) {
     fecha = new Date();
     fecha.setDate(fecha.getDate() - dias);
     return fecha;
@@ -88,19 +74,30 @@ const restarDias = (dias) => {
 //crea un objeto gasto tomando los parametros ingresados en el form
 const crearGasto = () => {
     
-    fecha = new Date(document.getElementById("inputDate").value);    
+    let fecha = new Date(document.getElementById("inputDate").value);    
     fecha = fecha.toLocaleDateString();
-    categoria = document.getElementById("inputTypeExpense").value;
-    valor = parseFloat(document.getElementById("inputValueExpense").value);
-    remito = document.getElementById("inputTicketNumberExpense").value;
-    pago = document.getElementById("inputPaymentMethod").value;
+    let categoria = document.getElementById("inputTypeExpense").value;
+    let valor = parseFloat(document.getElementById("inputValueExpense").value);
+    let remito = document.getElementById("inputTicketNumberExpense").value;
+    let pago = document.getElementById("inputPaymentMethod").value;
 
     let nuevoGasto = new Gasto(fecha, categoria, valor,remito,pago);
     arrayGastos.push(nuevoGasto);
 
     document.getElementById("inputExpenseForm").reset();
+    nuevoGasto.storeExpense();
     addRowTable(nuevoGasto);
+
  }
+
+ function updateCanvas(objeto){
+    let canvas = document.getElementById("textSelectedRow");
+    let context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "40px Arial";
+    context.fillText(`$ ${objeto.valor}`, 100, 50);
+}
+
 
 //funcion agrega fila de tabla con nuevo gasto  
 function addRowTable(objeto){
@@ -131,11 +128,15 @@ function addRowTable(objeto){
                 nodofila.appendChild(objeto.returnNodoCeldaTabla(atributo)); 
             }
         }
+        /*
+        let enJSON = JSON.stringify(objeto);
+        localStorage.setItem(objeto.ID,enJSON);*/
 }
 //Devuelve la posicion del gasto maximo
 const maxGasto = () => {
     const valores = arrayGastos.map((object) => object.valor);
     max = Math.max.apply(null,valores);
+    localStorage.setItem("MAX_EXPENSE", JSON.stringify(max));
     return valores.indexOf(max);
 }
 
@@ -143,13 +144,17 @@ const maxGasto = () => {
 const minGasto = () => {
     const valores = arrayGastos.map((object) => object.valor);
     min = Math.min.apply(null,valores);
+    localStorage.setItem("MIN_EXPENSE", JSON.stringify(min));
     return valores.indexOf(min);
 }
 //Devuelve el promedio de gastos ingresados
 const promedioGastos = () => {
     let suma_valores = 0;
+    let prom = 0;
     arrayGastos.forEach(function(gasto) { suma_valores += gasto.valor });
-    return suma_valores/arrayGastos.length;
+    prom = (suma_valores/arrayGastos.length);
+    localStorage.setItem("AVERAGE_EXPENSES", JSON.stringify(prom));
+    return prom;
 }
 //Muestra calculos realizados
 const mostrarCalculos = () => {
@@ -164,6 +169,8 @@ const mostrarCalculos = () => {
     } else {
         mensajeCalculos += 'No es posible realizar calculos debido a que no se ingresaron gastos'
         alert(mensajeCalculos);
+
+
     }
 }
 
@@ -212,53 +219,48 @@ function mostrarGastos() {
     }
 }
 // se ingresan automaticamente 9 gastos para poder utilizar funciones de calculo
-for(let i=0;i<3;i++)
-{
-    let valor = Math.floor(Math.random() * 500);
-    let fecha = new Date();
-    nuevoGasto = new Gasto(fecha.toLocaleDateString(), "A", valor,`RECIBO${i}`,"Efec.");
-    arrayGastos.push(nuevoGasto);
-    addRowTable(nuevoGasto);
+
+function randomExpenses(){
+
+    for(let i=0;i<3;i++)
+    {
+        let valor = Math.floor(Math.random() * 500);
+        let fecha = new Date();
+        nuevoGasto = new Gasto(fecha.toLocaleDateString(), "A", valor,`RECIBO${i}`,"Efec.");
+        arrayGastos.push(nuevoGasto);
+        nuevoGasto.storeExpense();
+        addRowTable(nuevoGasto);
+    }
+    for(let i=3;i<6;i++)
+    {
+        let valor = Math.floor(Math.random() * 500);
+        let fecha = new Date();
+        nuevoGasto = new Gasto(fecha.toLocaleDateString(), "B", valor,`RECIBO${i}`,"Transf.");
+        arrayGastos.push(nuevoGasto);
+        nuevoGasto.storeExpense();
+        addRowTable(nuevoGasto);
+    }
+    for(let i=6;i<9;i++)
+    {
+        let valor = Math.floor(Math.random() * 500);
+        let fecha = new Date();
+        nuevoGasto = new Gasto(fecha.toLocaleDateString(), "C", valor,`RECIBO${i}`,"Efec.");
+        arrayGastos.push(nuevoGasto);
+        nuevoGasto.storeExpense();
+        addRowTable(nuevoGasto);
+    }
 }
-for(let i=3;i<6;i++)
-{
-    let valor = Math.floor(Math.random() * 500);
-    let fecha = new Date();
-    nuevoGasto = new Gasto(fecha.toLocaleDateString(), "B", valor,`RECIBO${i}`,"Transf.");
-    arrayGastos.push(nuevoGasto);
-    addRowTable(nuevoGasto);
-}
-for(let i=6;i<9;i++)
-{
-    let valor = Math.floor(Math.random() * 500);
-    let fecha = new Date();
-    nuevoGasto = new Gasto(fecha.toLocaleDateString(), "C", valor,`RECIBO${i}`,"Efec.");
-    arrayGastos.push(nuevoGasto);
-    addRowTable(nuevoGasto);
-}
-
-//Declaracion de Eventos
-
-let btmInputExpenseForm = document.getElementById("btmInputExpenseForm");
-btmInputExpenseForm.onclick = () =>{crearGasto()};
-
-let btmMostrarCalculos = document.getElementById("btmMostrarCalculos");
-btmMostrarCalculos.onclick = () =>{mostrarCalculos()};
-
-let btmFiltrar = document.getElementById("btmFiltrar");
-btmFiltrar.onclick = () =>{filtrar()};
-
-let btmReset= document.getElementById("btmReset");
-btmReset.onclick = () =>{resetGastos()};
-
-//funcion que actualiza canvas
-
-function updateCanvas(objeto){
-    let canvas = document.getElementById("textSelectedRow");
-    let context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.font = "40px Arial";
-    context.fillText(`$ ${objeto.valor}`, 100, 50);
-}
-
-
+    //Declaracion de Eventos
+    
+    let btmInputExpenseForm = document.getElementById("btmInputExpenseForm");
+    btmInputExpenseForm.onclick = () =>{crearGasto()};
+    
+    let btmMostrarCalculos = document.getElementById("btmMostrarCalculos");
+    btmMostrarCalculos.onclick = () =>{mostrarCalculos()};
+    
+    let btmFiltrar = document.getElementById("btmFiltrar");
+    btmFiltrar.onclick = () =>{filtrar()};
+    
+    let btmReset= document.getElementById("btmReset");
+    btmReset.onclick = () =>{resetGastos()};
+    
